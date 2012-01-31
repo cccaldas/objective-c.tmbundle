98d749f34057b31df3d8717d07a1c912cefe9bac
Cristiano Caldas <eu@cccaldas.com>
2012-01-31 18:40:11 -0200

    class template done, testing

---
+++ Templates/Objective-c Class.tmTemplate/implementation.m
@@ -0,0 +1,12 @@
+//
+//  ${TM_NEW_FILE_BASENAME}.m
+//
+//  Created by ${TM_FULLNAME} on ${TM_DATE}.
+//  Copyright (c) ${TM_YEAR} ${TM_ORGANIZATION_NAME}. All rights reserved.
+//
+
+#import "${TM_NEW_FILE_BASENAME}.h"
+
+@implementation ${TM_NEW_FILE_BASENAME}
+	
+@end
---
+++ Templates/Objective-c Class.tmTemplate/interface.h
@@ -0,0 +1,13 @@
+//
+//  ${TM_NEW_FILE_BASENAME}.h
+//
+//  Created by ${TM_FULLNAME} on ${TM_DATE}.
+//  Copyright (c) ${TM_YEAR} ${TM_ORGANIZATION_NAME}. All rights reserved.
+//
+
+@interface ${TM_NEW_FILE_BASENAME} : NSObject
+{
+	
+}
+
+@end
